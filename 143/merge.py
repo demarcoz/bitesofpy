5a9632c6e06738c4288ed80381ed30c0f3065dc9
Marco de Zeeuw <marco@dezeeuw.nl>
2019-11-24 16:49:13 +0100

    submission Bite 143 @ codechalleng.es

---
+++ 143/merge.py
@@ -0,0 +1,21 @@
+NOT_FOUND = "Not found"
+
+group1 = {'tim': 30, 'bob': 17, 'ana': 24}
+group2 = {'ana': 26, 'thomas': 64, 'helen': 26}
+group3 = {'brenda': 17, 'otto': 44, 'thomas': 46}
+
+
+def get_person_age(name):
+    """Look up name (case insensitive search) and return age.
+       If name in > 1 dict, return the match of the group with
+       greatest N (so group3 > group2 > group1)
+    """
+    if isinstance(name, str):
+        name = name.lower()
+    if name in group3.keys():
+        return group3[name]
+    if name in group2.keys():
+        return group2[name]
+    if name in group1.keys():
+        return group1[name]
+    return NOT_FOUND
