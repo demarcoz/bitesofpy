942dabfe50f51622a53419e58e3616b35968bbb7
Marco de Zeeuw <marco.zeeuw@gmail.com>
2020-10-03 15:18:32 +0200

    submission Bite 153 @ codechalleng.es

---
+++ 153/rounding.py
@@ -0,0 +1,13 @@
+import math
+
+def round_up_or_down(transactions, up=True):
+    """Round the list of transactions passed in.
+       If up=True (default) round up, else round down.
+       Return a new list of rounded values
+    """
+    if up:
+       rounder = math.ceil
+    else:
+       rounder = math.floor
+
+    return [rounder(x) for x in transactions]
