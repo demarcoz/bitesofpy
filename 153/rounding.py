405b8bd3626348c1f8bfc13aea28183784ec961e
Marco de Zeeuw <marco.zeeuw@gmail.com>
2020-10-03 15:43:16 +0200

    fix formatting

--- 153/rounding.py
+++ 153/rounding.py
@@ -1,13 +1,14 @@
 import math
 
+
 def round_up_or_down(transactions, up=True):
     """Round the list of transactions passed in.
-       If up=True (default) round up, else round down.
-       Return a new list of rounded values
+    If up=True (default) round up, else round down.
+    Return a new list of rounded values
     """
     if up:
-       rounder = math.ceil
+        rounder = math.ceil
     else:
-       rounder = math.floor
+        rounder = math.floor
 
     return [rounder(x) for x in transactions]
