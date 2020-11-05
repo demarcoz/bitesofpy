6e496a4c3f8a2076a0d8937378d5aa06e4d3eadf
Marco de Zeeuw <marco@dezeeuw.nl>
2020-11-05 20:01:28 +0100

    submission Bite 246 @ codechalleng.es

--- 246/test_workouts.py
+++ 246/test_workouts.py
@@ -6,15 +6,15 @@ from workouts import print_workout_days
 @pytest.mark.parametrize("value, expected", [
     ("body", "Mon, Tue, Thu, Fri\n"),
     ("lower", "Tue, Fri\n"),
-    ("upper", "Mon, Tue\n"),
+    ("upper", "Mon, Thu\n"),
     ("cardio", "Wed\n"),
     ("", "Mon, Tue, Wed, Thu, Fri\n"),
     ("none", "No matching workout\n"),
 ])
-def test_print_workout_days(capsys, input, output):
-    print_workout_days(input)
+def test_print_workout_days(capsys, value, expected):
+    print_workout_days(value)
     capture = capsys.readouterr()
-    assert capture.out == output
+    assert capture.out == expected
 
 
 def test_typeerror():
