1ee12865192b1562c441721046d9c03ace11026c
Marco de Zeeuw <marco.zeeuw@gmail.com>
2020-11-11 19:35:06 +0100

    improved solution

--- 257/users.py
+++ 257/users.py
@@ -8,12 +8,10 @@ def get_users(passwd: str) -> dict:
       replace multiple commas in name with a single space
       return dict of keys = user, values = name.
     """
-    lines = passwd.strip().splitlines()
-    users = {}
-    for line in lines:
-        user, _, _, _, name, _, _ = line.split(":")
-        name = re.sub(",+$", "", name)
-        name = re.sub(",+", " ", name)
-        name = "unknown" if name == "" else name
-        users[user] = name
-    return users
+    output = {}
+    for row in passwd.strip().splitlines():
+        fields = row.split(':')
+        username = fields[0]
+        name = re.sub(r',+', r' ', fields[4].strip(',')) or 'unknown'
+        output[username] = name
+    return output
