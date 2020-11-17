e2852302b7c233dcabd83a58cd9cb89717b2da51
Marco de Zeeuw <marco.zeeuw@gmail.com>
2020-11-17 19:48:14 +0100

    submission Bite 293 @ codechalleng.es

---
+++ 293/n_digit_numbers.py
@@ -0,0 +1,8 @@
+from typing import List, TypeVar
+T = TypeVar('T', int, float)
+
+
+def n_digit_numbers(numbers: List[T], n: int) -> List[int]:
+    if n < 1:
+        raise ValueError
+    return [int(_ * 10**(n - len(str(abs(round(_)))))) for _ in numbers]
