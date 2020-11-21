567a235c1496df82cb66078698f22a1ff29c4830
Marco de Zeeuw <marco.zeeuw@gmail.com>
2020-11-21 16:00:38 +0100

    submission Bite 4 @ codechalleng.es

---
+++ 4/tags.py
@@ -0,0 +1,21 @@
+import os
+import re
+from collections import Counter
+import urllib.request
+
+# prep
+tempfile = os.path.join('/tmp', 'feed')
+urllib.request.urlretrieve(
+    'https://bites-data.s3.us-east-2.amazonaws.com/feed',
+    tempfile
+)
+
+with open(tempfile) as f:
+    content = f.read().lower()
+
+
+def get_pybites_top_tags(n=10):
+    """use Counter to get the top 10 PyBites tags from the feed
+       data already loaded into the content variable"""
+    matches = re.findall('<category>(\w+)</category>', content)
+    return Counter(matches).most_common(n)
