3760a055b9ef5574ae09c8280efaa218baffc1f4
Marco de Zeeuw <marco@dezeeuw.nl>
2020-11-24 20:54:08 +0100

    submission Bite 10 @ codechalleng.es

---
+++ 10/divide.py
@@ -0,0 +1,8 @@
+def positive_divide(numerator, denominator):
+    try:
+        result = numerator / denominator
+        if result < 0:
+            raise ValueError
+        return result
+    except ZeroDivisionError:
+        return 0
