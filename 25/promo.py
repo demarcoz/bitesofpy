85a35f7749922406a3b1c45a1ad9c3214fca75c5
Marco de Zeeuw <marco.zeeuw@gmail.com>
2020-12-01 18:12:01 +0100

    submission Bite 25 @ codechalleng.es

---
+++ 25/promo.py
@@ -0,0 +1,48 @@
+import random
+
+BITES = {6: 'PyBites Die Hard',
+         7: 'Parsing dates from logs',
+         9: 'Palindromes',
+         10: 'Practice exceptions',
+         11: 'Enrich a class with dunder methods',
+         12: 'Write a user validation function',
+         13: 'Convert dict in namedtuple/json',
+         14: 'Generate a table of n sequences',
+         15: 'Enumerate 2 sequences',
+         16: 'Special PyBites date generator',
+         17: 'Form teams from a group of friends',
+         18: 'Find the most common word',
+         19: 'Write a simple property',
+         20: 'Write a context manager',
+         21: 'Query a nested data structure'}
+BITES_DONE = {6, 10, 16, 18, 21}
+
+
+class NoBitesAvailable(Exception):
+    pass
+
+
+class Promo:
+
+    def __init__(self):
+        # updated Bite to make local copies (avoid globals!)
+        self.all_bites = BITES.copy()
+        self.bites_done = BITES_DONE.copy()
+
+    def _pick_random_bite(self):
+        """Pick a random Bite that is not done yet, if all
+           Bites are done, raise a NoBitesAvailable exception"""
+        bites = list(self.all_bites.keys())
+        for bite in self.bites_done:
+            bites.remove(bite)
+        try:
+            return random.choice(bites)
+        except Exception:
+            raise NoBitesAvailable
+
+    def new_bite(self):
+        """Get  a random Bite using _pick_random_bite,
+           add it to self.bites_done, then return it"""
+        random_bite = self._pick_random_bite()
+        self.bites_done.update({random_bite})
+        return random_bite
